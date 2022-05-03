817a0efa712675e685d28b158a26dac8870ff857
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-03 15:40:40 +0700

    about and error pages

--- setup/src/pages/About.js
+++ setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem est
+        officia, temporibus porro molestiae harum doloribus impedit pariatur
+        magnam repudiandae earum, adipisci asperiores necessitatibus
+        reprehenderit cumque maxime nostrum. Non harum labore ducimus rerum eum
+        qui recusandae, itaque consequatur cumque in sit at. Veniam, voluptates.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
